9c415feba06c89b032577f917d4eba5c70b4ca80
Ashish Singh <aitashish173@gmail.com>
2017-10-23 13:19:29 +0530

    Changes:-
    * Done with carousel header block
    * Fixes for order of the images in split carousel

--- src/js/split-carousel.js
+++ src/js/split-carousel.js
@@ -1,8 +1,17 @@
-currentIndex = $('.carousel-item.active').find('img').attr('src');
+// First Index
+currentIndex = $('.carousel-item.active').next('.carousel-item').find('img').attr('src');
 
-$('#carousel-wrapper').on('slide.bs.carousel', function () {
-  currentIndex = $('.carousel-item.active').find('img').attr('src');
+$('#carousel-wrapper').on('slid.bs.carousel', function () {
+  currentIndex = $('.carousel-item.active').next('.carousel-item');
+  var e = currentIndex.find('img').attr('src');
+  // Last Index
+  if(e === 'undefined' || e === undefined) {
+    currentIndex = $('.carousel-item').first('.carousel-item').find('img').attr('src');
+  } else {
+    currentIndex = currentIndex.find('img').attr('src');
+  }
   $('a.carousel-control-next').css('background-image', 'url(' + currentIndex + ')');
+
 });
 
   $('a.carousel-control-next').css('background-image', 'url(' + currentIndex + ')');
