70e48df32fd8809c29e05a7f84528d6dd3757b70
Alex Cavender <alexcavender@gmail.com>
2017-11-29 13:43:35 -0500

    mobilefirst true for slider

--- custom.js
+++ custom.js
@@ -208,9 +208,30 @@ $('#carousel-wrapper-not-header').on('slid.bs.carousel', function () {
 
 $('.publications-slider').slick({
   infinite: false,
+  mobileFirst: true,
   slidesToShow: 2,
   slidesToScroll: 1,
   arrows: false,
-  dots: false
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
   
