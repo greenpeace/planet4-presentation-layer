b2b09460cc36d4b85b12e2ee1dcd5867e136163a
Alex Cavender <alexcavender@gmail.com>
2017-11-30 20:47:25 -0500

    publications slider output into custom.js

--- custom.js
+++ custom.js
@@ -163,6 +163,36 @@ if($( window ).width() <= 768) {
   });
 };
 $(function() {
+  $('.publications-slider').slick({
+    infinite: false,
+    mobileFirst: true,
+    slidesToShow: 2.2,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: "unslick"
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+$(function() {
   $('#search-type button').click(function() {
      $('#search-type button').removeClass("active");
     $(this).addClass("active");
@@ -188,33 +218,5 @@ $('#carousel-wrapper-header').on('slid.bs.carousel', function () {
 
 });
 
-$('#carousel-wrapper-header a.carousel-control-next').css('background-image', 'url(' + currentIndexheader + ')');
+  $('#carousel-wrapper-header a.carousel-control-next').css('background-image', 'url(' + currentIndexheader + ')');
 
-$('.publications-slider').slick({
-  infinite: false,
-  mobileFirst: true,
-  slidesToShow: 2.2,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: "unslick"
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
