d441340dcfe2d63b1c15d426b429676ff88a39f8
Alex Cavender <alexcavender@gmail.com>
2017-12-02 12:15:21 -0500

    use tabs on pubslider

--- src/js/pubslider.js
+++ src/js/pubslider.js
@@ -1,30 +1,30 @@
 $(function() {
-  $('.publications-slider').slick({
-    infinite: false,
-    mobileFirst: true,
-    slidesToShow: 2.2,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: false,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+	$('.publications-slider').slick({
+		infinite: false,
+		mobileFirst: true,
+		slidesToShow: 2.2,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false,
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: "unslick"
+			},
+			{
+				breakpoint: 768,
+				settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
 });
