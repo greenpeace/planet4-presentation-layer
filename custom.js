84b45c777a73cc0742061ed57a8a75ae4ce9e1c1
Nikos Roussos <nikos@roussos.cc>
2018-01-09 10:38:40 +0200

    Remove unused search code

--- custom.js
+++ custom.js
@@ -356,13 +356,4 @@ $(function() {
 });
 
 
-$(function() {
-  $('#search-type button').click(function() {
-     $('#search-type button').removeClass("active");
-    $(this).addClass("active");
-    });
 
-  $('.btn-filter').click(function() {
-  	$('#filtermodal').modal('show');
-    });
-});
