0e96b5ae5f3c46f31c71cc9487d792bc01ca3d77
Nikos Roussos <nikos@roussos.cc>
2018-01-22 14:09:00 +0200

    Force post action card to follow scroll (#204)

---
+++ src/js/post-action.js
@@ -0,0 +1,19 @@
+$(function() {
+  // Force the Cover card to follow scroll
+  var $sidebar   = $("#action-card");
+  var $window    = $(window);
+  var offset    = $sidebar.offset();
+  var topPadding = 100;
+
+  $window.scroll(function() {
+    if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
+      $sidebar.stop().animate({
+        marginTop: $window.scrollTop() - offset.top + topPadding
+      });
+    } else {
+      $sidebar.stop().animate({
+        marginTop: 0
+      });
+    }
+  });
+});
