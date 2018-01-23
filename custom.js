2a26775b71d4b4ac4ef46548342bc9b438e91398
Kiriakos Diamantis <replicator31@hotmail.com>
2018-01-23 11:33:34 +0200

    Add check for #action-card elements before adding scroll event handler.

--- custom.js
+++ custom.js
@@ -339,20 +339,22 @@ $(function() {
   // Force the Cover card to follow scroll
   var $sidebar   = $("#action-card");
   var $window    = $(window);
-  var offset    = $sidebar.offset();
+  var offset     = $sidebar.offset();
   var topPadding = 100;
 
-  $window.scroll(function() {
-    if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
-      $sidebar.stop().animate({
-        marginTop: $window.scrollTop() - offset.top + topPadding
+  if ($sidebar.size() > 0) {
+      $window.scroll(function () {
+          if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
+              $sidebar.stop().animate({
+                  marginTop: $window.scrollTop() - offset.top + topPadding
+              });
+          } else {
+              $sidebar.stop().animate({
+                  marginTop: 0
+              });
+          }
       });
-    } else {
-      $sidebar.stop().animate({
-        marginTop: 0
-      });
-    }
-  });
+  }
 });
 
 $(function() {
