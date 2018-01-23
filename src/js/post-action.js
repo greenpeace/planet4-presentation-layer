2610004b3553cdc0a743225936d48066d979094d
Kiriakos Diamantis <replicator31@hotmail.com>
2018-01-23 12:13:37 +0200

    Updated also post-action.js for scroll event handler.

--- src/js/post-action.js
+++ src/js/post-action.js
@@ -1,19 +1,21 @@
 $(function() {
-  // Force the Cover card to follow scroll
-  var $sidebar   = $("#action-card");
-  var $window    = $(window);
-  var offset    = $sidebar.offset();
-  var topPadding = 100;
+    // Force the Cover card to follow scroll
+    var $sidebar   = $("#action-card");
+    var $window    = $(window);
+    var offset     = $sidebar.offset();
+    var topPadding = 100;
 
-  $window.scroll(function() {
-    if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
-      $sidebar.stop().animate({
-        marginTop: $window.scrollTop() - offset.top + topPadding
-      });
-    } else {
-      $sidebar.stop().animate({
-        marginTop: 0
-      });
+    if ($sidebar.size() > 0) {
+        $window.scroll(function () {
+            if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
+                $sidebar.stop().animate({
+                    marginTop: $window.scrollTop() - offset.top + topPadding
+                });
+            } else {
+                $sidebar.stop().animate({
+                    marginTop: 0
+                });
+            }
+        });
     }
-  });
 });
