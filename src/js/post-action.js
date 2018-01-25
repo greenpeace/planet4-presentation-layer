e47f66bbc916332e2cc1469e88f220a4013ceb1a
Nikos Roussos <nikos@roussos.cc>
2018-01-25 13:27:11 +0200

    Take action box fixes (#213)
    * Stop fixed behaviour at the end of the post
    * Fix padding of post-content even when take action box is missing

--- src/js/post-action.js
+++ src/js/post-action.js
@@ -6,12 +6,17 @@ $(function() {
   var topPadding = 100;
 
   if ($sidebar.length > 0) {
+    var absPosition = $('.post-details > p:last-child').offset().top - $sidebar.outerHeight() - topPadding;
+
     $window.scroll(function () {
-      if ($window.scrollTop() > offset.top && $sidebar.css('float') != 'none') {
+      if ($window.scrollTop() > offset.top &&
+          $window.scrollTop() < absPosition &&
+          $sidebar.css('float') != 'none') {
         $sidebar.stop().animate({
           marginTop: $window.scrollTop() - offset.top + topPadding
         });
-      } else {
+      }
+      if ($window.scrollTop() < offset.top) {
         $sidebar.stop().animate({
           marginTop: 0
         });
